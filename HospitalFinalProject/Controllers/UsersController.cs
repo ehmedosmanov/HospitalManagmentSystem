fb9a70e2907749e0b9e585701bd2c090afbcc998
ehmedosmanov <ehmedosmanov@gmail.com>
2023-09-23 00:47:57 +0400

    Created UsersController

--- HospitalFinalProject/Controllers/UsersController.cs
+++ HospitalFinalProject/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace HospitalFinalProject.Controllers
@@ -13,16 +14,17 @@ namespace HospitalFinalProject.Controllers
     [Authorize(Roles = "Admin")]
     public class UsersController : Controller
     {
+        #region Identity
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly UserManager<AppUser> _userManager;
-        private readonly AppDbContext _db;
-        public UsersController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager, AppDbContext db)
+        public UsersController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
             _userManager = userManager;
-            _db = db;
         }
+        #endregion
 
+        #region Index
         public async Task<IActionResult> Index()
         {
             List<AppUser> users = await _userManager.Users.ToListAsync();
@@ -33,23 +35,223 @@ namespace HospitalFinalProject.Controllers
                 UserVM userVM = new UserVM()
                 {
                     Id = user.Id,
-                    Name = user.UserName,
+                    Name = user.Name,
                     Surname = user.Surname,
                     Username = user.UserName,
                     Email = user.Email,
                     Gender = user.Gender,
                     IsDeactive = user.IsDeactive,
-                    Role = (await _userManager.GetRolesAsync(user))[0]
+                    Role = (await _userManager.GetRolesAsync(user)).FirstOrDefault() ?? "No Role"
                 };
                 userVMs.Add(userVM);
             }
 
             return View(userVMs);
         }
+        #endregion
 
-        public IActionResult Create()
+        #region Create
+        public async Task<IActionResult> Create()
         {
+            ViewBag.Roles = await _roleManager.Roles.ToListAsync();
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RegisterVM registerVM, string role)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerVM);
+            }
+
+            ViewBag.Roles = await _roleManager.Roles.ToListAsync();
+
+            AppUser appUser = new AppUser()
+            {
+                Name = registerVM.Name,
+                Surname = registerVM.Surname,
+                Email = registerVM.Email,
+                UserName = registerVM.Username,
+                Gender = registerVM.Gender,
+            };
+            IdentityResult identityResult = await _userManager.CreateAsync(appUser, password: registerVM.Password);
+
+            if (!identityResult.Succeeded)
+            {
+                foreach (IdentityError error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(registerVM);
+            }
+            await _userManager.AddToRoleAsync(appUser, role);
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Update
+        public async Task<IActionResult> Update(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            AppUser user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            UpdateVM updateVM = new()
+            {
+                Name = user.Name,
+                Surname = user.Surname,
+                Username = user.UserName,
+                Email = user.Email,
+                Gender = user.Gender,
+                Role = (await _userManager.GetRolesAsync(user)).FirstOrDefault(),
+            };
+            ViewBag.Roles = await _roleManager.Roles.ToListAsync();
+            return View(updateVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(string id, UpdateVM updateVM, string role)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // Id-ye görə istifadəçini tapırıq
+            AppUser user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            UpdateVM dbUpdateVM = new()
+            {
+                Name = user.Name,
+                Surname = user.Surname,
+                Username = user.UserName,
+                Email = user.Email,
+                Gender = user.Gender,
+                Role = (await _userManager.GetRolesAsync(user)).FirstOrDefault(),
+            };
+
+            // Rolların siyahısını əldə edirik və ViewBag vasitəsilə ötürürük.
+            ViewBag.Roles = await _roleManager.Roles.ToListAsync();
+
+
+            user.Email = updateVM.Email;
+            user.UserName = updateVM.Username;
+            user.Name = updateVM.Name;
+            user.Surname = updateVM.Surname;
+            user.Gender = updateVM.Gender;
+
+            await _userManager.UpdateAsync(user);
+
+            if (role != dbUpdateVM.Role)
+            {
+                IdentityResult addIdentityResult = await _userManager.AddToRoleAsync(user, role);
+                if (!addIdentityResult.Succeeded)
+                {
+                    foreach (IdentityError error in addIdentityResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View();
+                }
+                IdentityResult removeIdentityResult = await _userManager.RemoveFromRoleAsync(user, role: dbUpdateVM.Role);
+                if (!removeIdentityResult.Succeeded)
+                {
+                    foreach (IdentityError error in removeIdentityResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Activity
+        public async Task<IActionResult> Activity(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            AppUser dbUser = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (dbUser == null)
+            {
+                return BadRequest();
+            }
+
+            if (dbUser.IsDeactive)
+            {
+                dbUser.IsDeactive = false;
+            }
+            else
+            {
+                dbUser.IsDeactive = true;
+            }
+            await _userManager.UpdateAsync(dbUser);
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region ResetPassword
+        public async Task<IActionResult> ResetPassword(string id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            AppUser user = await _userManager.FindByIdAsync(id);
+            
+            if(user == null) 
+            {
+                return BadRequest();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ResetPassword(string id, ResetPasswordVM resetPasswordVM)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            AppUser user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            string token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            IdentityResult identityResult = await _userManager.ResetPasswordAsync(user, token, resetPasswordVM.Password);
+
+            if (!identityResult.Succeeded)
+            {
+                foreach (IdentityError error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View();
+            }
+            return RedirectToAction("Index");
+
+        }
+        #endregion
     }
 }
