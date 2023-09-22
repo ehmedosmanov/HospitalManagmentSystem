7d2cae52f791202e23677b68a6c1cbcc957e1b0c
ehmedosmanov <ehmedosmanov@gmail.com>
2023-09-23 00:44:31 +0400

    Created ForgotPassword ViewModel

---
+++ HospitalFinalProject/ViewModels/ForgotPasswordVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalFinalProject.ViewModels
+{
+    public class ForgotPasswordVM
+    {
+        [Required(ErrorMessage = "Email tələb olunur.")]
+        [EmailAddress(ErrorMessage = "Zəhmət olmasa düzgün email qeyd edin.")]
+        public string Email { get; set; }
+    }
+}
